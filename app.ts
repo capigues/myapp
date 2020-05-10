c9068b8346fbd4ca5991fb43fa5baa83ade0e88c
Caleb <capigues@aggies.ncat.edu>
2020-05-10 17:28:43 -0400

    testing 2

--- app.ts
+++ app.ts
@@ -4,3 +4,21 @@ const button = document.querySelector('button')!;
 button.addEventListener('click', () => {
     console.log('Clicked!');
 });
+/*
+git checkout
+git add .
+git commit -m
+git status
+git clone
+git diff
+git merge
+git pull - uses remote repository
+git push - uses remote repository
+git rebase 
+git reset
+git restore
+git revert
+git show - shows commits
+git switch
+
+*/
